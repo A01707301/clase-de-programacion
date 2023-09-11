ef44cc2a1cc384b7242f3fa2872cc22c9e803894
A01707301 <142333609+A01707301@users.noreply.github.com>
2023-09-10 23:04:52 -0600

    Update Calculadora.py

--- Calculadora.py
+++ Calculadora.py
@@ -30,6 +30,14 @@ def calcular_fuerza_friccion(coeficiente_friccion_estatica, fuerza_normal):
     fuerza_friccion = coeficiente_friccion_estatica * fuerza_normal
     return fuerza_friccion
 
+def calcular_suma_nc(suma_nc1, suma_nc2):
+    suma_nc = suma_nc1 + suma_nc2
+    return suma_nc
+
+def calcular_resta_nc(resta_nc1, resta_nc2):
+    resta_nc = resta_nc1 - resta_nc2
+    return resta_nc
+
 def calcular_km_a_m(kilometro):
     km_a_m = kilometro * 1000
     return km_a_m
@@ -43,7 +51,7 @@ def calcular_kg_a_g(kilogramo):
     return kg_a_g
 
 def calcular_g_a_kg(gramo):
-    g_a_kg = gram / 1000 
+    g_a_kg = gramo / 1000 
     return g_a_kg
 
 def calcular_m_a_s(minuto):
@@ -54,31 +62,59 @@ def calcular_s_a_m(segundo):
     s_a_m = segundo / 60 
     return s_a_m
 
-print("¿Quieres usar el formulario?, ¿Quieres resolver una ecuación? o ¿Quieres hacer conversión de unidades?")
-print("1. Formulario")
-print("2. Resolver la ecuación")
+print("¿Qué deseas hacer?")
+print("1. Usar el formulario")
+print("2. Resolver una ecuación")
 print("3. Conversión de unidades")
 
 calculadora = int(input("Selecciona una opción (1/2/3): "))
 
 if calculadora == 1:
     print("Formulario de física")
-    print("1. Segunda Ley de Newton")
-    print("2. Aceleración")
-    print("3. Fricción")
-    print("4. Fuerza centripeta")
+    print("1. Fuerzas")
+    print("2. Movimiento")
 
-    formulario = int(input("Selecciona una opción (1/2/3/4): "))
+    opcion_uno = int(input("Selecciona una opción (1/2/3): "))
 
-    if formulario == 1: 
-        Newton = input("F=ma")
-    elif formulario == 2: 
-        aceleracion = input(" \n a= Vf-Vo/tf-to \n a= Vf-Vo/t \n xf= xo + vot + 1/2 at2 ")
-    elif formulario == 3: 
-        friccion = input("\n Fr= μN \n Fr= μmg")
-    elif formulario == 4:
-        centripeta = input("\n F=mat \n F=v2/R")
-    else: 
+    if opcion_uno == 1:
+        print("1. Segunda Ley de Newton")
+        print("2. Aceleración")
+        print("3. Fricción")
+        print("4. Fuerza centripeta")
+
+        formulario = int(input("Selecciona una opción (1/2/3/4): "))
+
+        if formulario == 1: 
+            Newton = input("F=ma")
+        elif formulario == 2: 
+            aceleracion = input(" \n a= Vf-Vo/tf-to \n a= Vf-Vo/t \n xf= xo + vot + 1/2 at2 ")
+        elif formulario == 3: 
+            friccion = input("\n Fr= μN \n Fr= μmg")
+        elif formulario == 4:
+            centripeta = input("\n F=mat \n F=v2/R")
+        else: 
+            print("No insertaste un dato correcto, regresa al menú principal")
+
+    elif opcion_uno == 2:
+        print("1. Movimiento rectilíneo uniforme")
+        print("2. Movimiento parabólico")
+        print("3. Movimiento de caída libre")
+        print("4. Movimiento circular")
+
+        opcion_seis = int(input("Selecciona una opción (1/2/3/4): "))
+
+        if opcion_seis == 1:
+            rectilineo = input ("\n x=vt \n x=x0+vt \n v= x1-x2/t1-t2 \n v=v0+at \n x= x0+v0t+1/2 at2")
+        elif opcion_seis == 2:
+            parabolico = input ("\n vx=v0cosα \n vy=v0sina \n x(t)=x0+vxt \n y=y0 + vyt - 1/2gt2 \n y= vy0 -gt = v0 sin a -gt")
+        elif opcion_seis ==3:
+            caida_libre = input("y= y0 + v0t - 1/2 gt2 \n v=v0 - gt \n y= h - 1/2 gt2")
+        elif opcion_seis == 4:
+            circular = input ("∣v1∣=∣v2∣ \n w= θ2-θ1/t2-t1 \n w=dθ/dt \n w=2π/T \n v=wR \n ac= v2/R \n ac=w2R \n θf=θ0+wt \n w=w0 + at \n t=w - w0/a")
+        else: 
+            print("No insertaste un dato correcto, regresa al menú principal")
+
+    else:
         print("No insertaste un dato correcto, regresa al menú principal")
 
 elif calculadora == 2: 
@@ -86,8 +122,9 @@ elif calculadora == 2:
     print("1. Distancia, Velocidad o Tiempo")
     print("2. Fuerza, Masa, Aceleración")
     print("3. Fuerza normal o fuerza de fricción")
+    print("4. Notación científica Suma/Resta")
 
-    opciones = int(input("Selecciona una opción (1/2/3): "))
+    opciones = int(input("Selecciona una opción (1/2/3/4): "))
     if opciones == 1:
         print("1. Calcular velocidad")
         print("2. Calcular distancia")
@@ -162,8 +199,37 @@ elif calculadora == 2:
             fuerza_friccion = calcular_fuerza_friccion(coeficiente_friccion_estatica, fuerza_normal)
             print(f"La fuerza de fricción es: {fuerza_friccion} N ")
         
-        else: 
-            print("No insertaste un dato correcto, regresa al menú principal")
+    elif opciones == 4: 
+        print("1. Suma de notación científica")
+        print("2. Resta de notación científica")
+
+        opcion_siete = int(input("Selecciona una opción (1/2): "))
+        if opcion_siete == 1:
+            suma_nc1 = float(input("Insertar el primer número entero: "))
+            suma_nc_ex = float(input("Insertar el primer exponente: "))
+            suma_nc2 = float(input("Insertar el segundo número entero: "))
+            suma2_nc_ex = float(input("Insertar el segundo exponente: "))
+        
+            if suma_nc_ex == suma2_nc_ex:
+                suma_nc = calcular_suma_nc(suma_nc1, suma_nc2)
+                print(f"La suma es: {suma_nc} x10 {suma_nc_ex}")
+
+            if (suma_nc_ex < suma2_nc_ex) or (suma_nc_ex > suma2_nc_ex):
+                # Aquí falta manejar los casos en los que los exponentes son diferentes.
+        
+        if opcion_siete ==2:
+            resta_nc = float(input("Insertar el primer número entero: "))
+            resta_nc_ex = float(input("Insertar el primer exponente: "))
+            resta2_nc = float(input("Insertar el segundo número entero: "))
+            resta2_nc_ex = float(input("Insertar el segundo exponente: "))
+        
+            if resta_nc_ex == resta2_nc_ex:
+                resta_nc = calcular_resta_nc(resta_nc, resta2_nc)
+                print(f"La resta es: {resta_nc} x10 {resta_nc_ex}")
+                # Aquí falta manejar los casos en los que los exponentes son diferentes.
+                
+    else: 
+        print("No insertaste un dato correcto, regresa al menú principal")
 
 elif calculadora == 3:
     print("1. Convertir kilometros a metros")
@@ -209,7 +275,4 @@ elif calculadora == 3:
         print("No insertaste un dato correcto, regresa al menú principal")
 
 elif calculadora >= 4: 
-    print("No insertaste un dato correcto, regresa al menú principal")
-
-else: 
     print("No insertaste un dato correcto, regresa al menú principal")
